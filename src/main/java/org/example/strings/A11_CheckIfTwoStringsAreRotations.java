2fb4188214d1f93099131ebede547b8bfde06780
Nishad M <04nishad@gmail.com>
2026-06-19 18:56:15 +0530

    this is for 11th problem solved, previos commit was for solving 10th problem

---
+++ src/main/java/org/example/strings/A11_CheckIfTwoStringsAreRotations.java
@@ -0,0 +1,41 @@
+package org.example.strings;
+
+public class A11_CheckIfTwoStringsAreRotations {
+    public static void main(String[] args) {
+        String first = "nishad";
+        String second = "shadinasdas";
+
+
+        if(first.equals(second)){
+            System.out.println("Yes Rotations");
+            return;
+        }
+
+        if(first.length() != second.length()){
+            System.out.println("Not Rotations");
+            return;
+        }
+
+        StringBuilder s = new StringBuilder(second);
+        int count = 0;
+
+        for (int i = 0; i < s.length(); ) {
+            if(count == s.length() - 1){
+                System.out.println("Not rotation.");
+                return;
+            }
+            count++;
+
+            char firstChar = s.charAt(i);
+
+            s.deleteCharAt(i);
+
+            s.append(firstChar);
+
+            if(first.equals(s.toString())){
+                System.out.println("Yes two strings are rotations");
+                return;
+            }
+        }
+    }
+}
