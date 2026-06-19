70031ba9025e82aa6da454e21046a3ecbb3240ad
Nishad M <04nishad@gmail.com>
2026-06-19 17:44:55 +0530

    11th problem solved

--- src/main/java/org/example/strings/A10_FindTheCharacterThatAppearsMostFrequently.java
+++ src/main/java/org/example/strings/A10_FindTheCharacterThatAppearsMostFrequently.java
@@ -7,24 +7,24 @@ import java.util.Map;
 
 public class A10_FindTheCharacterThatAppearsMostFrequently {
     public static void main(String[] args) {
-        String s = "banana";
+        String s = "nishad";
 
         int maxCount = 0;
-        int newCount = 1;
 
         List<Character> compared = new ArrayList<>();
         Map<Character, Integer> counts = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
+            int newCount = 1;
             char mostRepeatedChar = s.charAt(i);
 
-            if(compared.contains(mostRepeatedChar)){
+            if (compared.contains(mostRepeatedChar)) {
                 continue;
             }
 
             compared.add(mostRepeatedChar);
 
-            for (int j = i+1; j < s.length(); j++) {
+            for (int j = i + 1; j < s.length(); j++) {
                 char c = s.charAt(j);
 
                 if (c == mostRepeatedChar) {
@@ -32,14 +32,23 @@ public class A10_FindTheCharacterThatAppearsMostFrequently {
                 }
             }
 
-            if(maxCount < newCount){
+            if (maxCount < newCount) {
                 maxCount = newCount;
             }
 
             counts.put(mostRepeatedChar, newCount);
         }
 
-        counts.get(maxCount);
+        if(maxCount == 1){
+            System.out.println("Characters not repeated more that once.");
+            return;
+        }
 
+        for(Map.Entry<Character, Integer> entry: counts.entrySet()){
+            if(entry.getValue() == maxCount){
+                System.out.println("The most frequently repeated character is : "+ entry.getKey() +" and it repeated "+ entry.getValue() + " times.");
+                return;
+            }
+        }
     }
 }
