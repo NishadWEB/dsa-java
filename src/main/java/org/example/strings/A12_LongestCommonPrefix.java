07bc747ba3a3c0339f406a1831f4d69c5b4ac05e
Nishad M <04nishad@gmail.com>
2026-06-20 19:03:26 +0530

    12th problem solved, this problem took a while to solve

---
+++ src/main/java/org/example/strings/A12_LongestCommonPrefix.java
@@ -0,0 +1,57 @@
+package org.example.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A12_LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] str = {"internet", "l", "interest"};
+        int length = str.length;
+
+        if (length == 0) {
+            System.out.println("EMpty array");
+            return;
+        }
+
+        if (length == 1) {
+            System.out.println("Only one string in this array");
+            return;
+        }
+
+        List<Character> prefix = new ArrayList<>();
+
+        int currCharIdx = 0;
+        int arrIdx = 0;
+        char c;
+        char globalChar = str[0].charAt(0);
+
+        while (true) {
+            if (arrIdx == (length - 1)) {
+                arrIdx = 0;
+                currCharIdx++;
+                prefix.add(globalChar);
+
+                try {
+                    globalChar = str[0].charAt(currCharIdx);
+                } catch (Exception e) {
+                    System.out.println("Longest common prefix is : " + prefix);
+                    return;
+                }
+            }
+
+            try {
+                c = str[arrIdx + 1].charAt(currCharIdx);
+            } catch (Exception e) {
+                System.out.println("Longest common prefix is : " + prefix);
+                return;
+            }
+
+            if (c == globalChar) {
+                arrIdx++;
+            } else {
+                System.out.println("Longest common prefix is : " + prefix);
+                return;
+            }
+        }
+    }
+}
